8da60dc98c15001c81798347a25447f93a545b1e
nitin3000ny <94776214+nitin3000ny@users.noreply.github.com>
2023-05-15 00:20:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const btn=document.getElementById('toggle');
-const menu=document.getElementById('nav-links');
-btn.addEventListener('click',()=>{
+const btn = document.getElementById('toggle');
+const menu = document.getElementById('nav-links');
+btn.addEventListener('click', () => {
     menu.classList.toggle('active');
 })
+
+const text = document.querySelector(".typing");
+
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = "Web Developer";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Web Designer";
+  }, 3500);
+  setTimeout(() => {
+    text.textContent = "Blogger";
+  }, 7700);
+};
+
+textLoad();
+setInterval(textLoad,12000)
+
