fbe94d171ee44eed39582d874338b3f157ceabb4
nitin3000ny <94776214+nitin3000ny@users.noreply.github.com>
2023-05-23 08:21:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,12 +12,15 @@ const textLoad = () => {
   }, 0);
   setTimeout(() => {
     text.textContent = "Web Designer";
+    text.style.color="#1db847";
   }, 4000);
   setTimeout(() => {
     text.textContent = "Content Writer";
+    text.style.color="#095587";
   }, 8000);
   setTimeout(() => {
     text.textContent = "Computer Engineer";
+    text.style.color="#095587";
   }, 12000);
 };
 
